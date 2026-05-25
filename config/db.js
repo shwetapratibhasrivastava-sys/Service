b7155112cb9c9ecc24f02ca41c408d1fce6a3945
Shweta Srivastava <shwetapratibhasrivastava@gmail.com>
2026-05-25 15:20:42 +0530

    done

--- config/db.js
+++ config/db.js
@@ -3,12 +3,14 @@ import mongoose from "mongoose";
 const connectDb = async () => {
   if (!process.env.MONGO_URI) {
     return console.log("MONGO_URI is not accessible");
+    process.exit(1)
   }
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDb connected");
   } catch (error) {
     console.log(error.message);
+    process.exit(1)
   }
 };
 
